9bd44373ab05226d101accb410ce3f8e984ad70f
katsuroom <kerrance.dai@gmail.com>
2023-11-13 10:27:13 -0500

    Added map card list to main screen

--- client/src/components/MainScreen.js
+++ client/src/components/MainScreen.js
@@ -1,11 +1,15 @@
 import TitleBar from "./TitleBar";
 import StatusBar from "./StatusBar";
+import MapCardList from "./MapCardList";
 
 export default function MainScreen()
 {
     return(
         <div>
             <TitleBar />
+            <div >
+                <MapCardList />
+            </div>
             <StatusBar />
         </div>
     );
