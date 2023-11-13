44acc0e1c29678575756350db77750878aa1348a
Weng-Sean <143642405+Weng-Sean@users.noreply.github.com>
2023-11-13 12:48:04 -0500

    Update auth.test.js

--- client/src/__tests__/auth.test.js
+++ client/src/__tests__/auth.test.js
@@ -1,4 +1,46 @@
-import apis from "../auth/auth-request-api";
+
+import axios from 'axios'
+// // const dotenv = require('dotenv')
+// require('dotenv').config({path:__dirname+'/./../../.env'})
+console.log("process.env.url", process.env.URL);
+axios.defaults.withCredentials = true;
+const baseURL = 'https://patrick-barbie-f64046e3bb4b.herokuapp.com/' + "auth"
+
+// const baseURL = "http://localhost:4000/auth"
+const api = axios.create({
+    baseURL: baseURL,
+})
+ 
+// THESE ARE ALL THE REQUESTS WE`LL BE MAKING, ALL REQUESTS HAVE A
+// REQUEST METHOD (like get) AND PATH (like /register). SOME ALSO
+// REQUIRE AN id SO THAT THE SERVER KNOWS ON WHICH LIST TO DO ITS
+// WORK, AND SOME REQUIRE DATA, WHICH WE WE WILL FORMAT HERE, FOR WHEN
+// WE NEED TO PUT THINGS INTO THE DATABASE OR IF WE HAVE SOME
+// CUSTOM FILTERS FOR QUERIES
+
+// export const getLoggedIn = () => api.get(`/loggedIn/`);
+export const loginUser = (email, password) => {
+    return api.post(`/login/`, {
+        email : email,
+        password : password
+    })
+}
+// export const logoutUser = () => api.get(`/logout/`)
+export const registerUser = (username, email, password) => {
+    console.log("inregister: ", baseURL);
+    return api.post(`/register/`, {
+        username : username,
+        email : email,
+        password : password,
+    })
+}
+const apis = {
+    // getLoggedIn,
+    registerUser,
+    loginUser,
+    // logoutUser
+}
+
 
 describe("Auth Test", () => {
   let username, email, password;
