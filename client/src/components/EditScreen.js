167b366d381d3b40821092f4e85258441286b4ee
ScottZhengGit <Scott.zheng@stonybrook.edu>
2023-11-13 10:23:41 -0500

    combined toolbar and propety

---
+++ client/src/components/EditScreen.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import PPolitical from "./PPolitical";
+import MapEditorToolbar from "./MapEditorToolBar";
+import StatusBar from "./StatusBar";
+import editScreenDemo from "../images/editScreenDemo.png"
+
+export default function EditScreen() {
+    const layoutStyle = {
+        display: 'flex',
+        flexDirection: 'column',
+        height: '100vh',
+        padding: '20vh 0',
+        boxSizing: 'border-box',
+    };
+
+    const toolbarStyle = {
+        width: '30%',
+        margin: '0',
+        marginLeft: '18%',
+    };
+
+    const imageStyle = {
+        width: '65%', // Set the image width to 50%
+        marginLeft: '1%',
+
+    };
+
+    const politicalStyle = {
+        width: '30%',
+        position: 'absolute',
+        top: '20%',
+        right: '0',
+        paddingBottom: '10%',
+    };
+
+    return (
+        <div style={layoutStyle}>
+            <div style={toolbarStyle}>
+                <MapEditorToolbar />
+            </div>
+            <img src={editScreenDemo} alt="Edit Screen Demo" style={imageStyle} /> {/* Apply the image style */}
+            <div style={politicalStyle}>
+                <PPolitical />
+            </div>
+        </div>
+    );
+}
