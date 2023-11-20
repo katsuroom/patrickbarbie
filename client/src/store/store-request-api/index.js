ad51e41e3e8508df45ef4f298b7f8e69ded113f5
lenzlaww <yuwenqian.chen@stonybrook.edu>
2023-11-19 19:38:45 -0500

    backend router curd

--- client/src/store/store-request-api/index.js
+++ client/src/store/store-request-api/index.js
@@ -1,100 +1,133 @@
-import axios from 'axios'
-axios.defaults.withCredentials = true;
-const baseURL = 'https://patrick-barbie-f64046e3bb4b.herokuapp.com/' + "api"
-const api = axios.create({
-    baseURL: baseURL,
-})
+// import axios from 'axios'
+// axios.defaults.withCredentials = true;
+// const baseURL = 'https://patrick-barbie-f64046e3bb4b.herokuapp.com/' + "api"
+// const api = axios.create({
+//     baseURL: baseURL,
+// })
 
-export const createMap = (mapData, userEmail, mapName) => {
-    return api.post(`/map/`, {
-        // SPECIFY THE PAYLOAD
-        name: mapName,
-        mapData: mapData,
-        ownerEmail: userEmail
+// export const createMap = (mapData, userEmail, mapName) => {
+//     return api.post(`/map/`, {
+//         // SPECIFY THE PAYLOAD
+//         name: mapName,
+//         mapData: mapData,
+//         ownerEmail: userEmail
+//     })
+// }
+
+// export const deleteMap = (mapId) => {
+//     return api.delete(`/map/${mapId}`)
+// }
+
+// export const updateMap = (mapId, mapData) => {
+//     return api.put(`/map/${mapId}`, {
+//         mapData: mapData
+//     })
+// }
+// // get one specific map
+// export const getMapById = (mapId) => {
+//     return api.get(`/map/${mapId}`)
+// }
+
+// //get all maps from one user
+// export const getMapList = (userId) => {
+//     return api.get(`/maplist/${userId}`)
+// }
+
+// //get all published maps from all users
+// export const getPublishedMapList = () => {
+//     return api.get(`/published-maplist/`)
+// }
+
+// //search map by map name in home page
+// export const searchMapByName = (mapName, userId) => {
+//     return api.get(`/searchmap-map-name/${userId}`, {
+//         params: {
+//             mapName: mapName
+//         }
+//     })
+// }
+
+// //search map by map name in community page
+// export const searchMapByNameInCommunity = (mapName) => {
+//     return api.get(`/searchmap-map-name-community/`, {
+//         params: {
+//             mapName: mapName
+//         }
+//     })
+// }
+
+// //search map by mapid
+// export const searchMapById = (mapId) => {
+//     return api.get(`/searchmap-mapid/`, {
+//         params: {
+//             mapId: mapId
+//         }
+//     })
+// }
+
+// //search map by property in home page
+// export const searchMapByProperty = (userId, property) => {
+//     return api.get(`/searchmap-property/${userId}`, {
+//         params: {
+//             property: property
+//         }
+//     })
+// }
+
+// //search map by property in community page
+// export const searchMapByPropertyInCommunity = (property) => {
+//     return api.get(`/searchmap-property-community/`, {
+//         params: {
+//             property: property
+//         }
+//     })
+// }
+
+// const apis = {
+//     createMap,
+//     deleteMap,
+//     updateMap,
+//     getMapById,
+//     getMapList,
+//     getPublishedMapList,
+//     searchMapByName,
+//     searchMapByNameInCommunity,
+//     searchMapById,
+//     searchMapByProperty,
+//     searchMapByPropertyInCommunity
+// }
+
+// export default apis;
+
+
+
+
+// DO NOT USE AXIOS
+
+// when you push to main, use the heroku url
+// const baseURL = 'https://patrick-barbie-f64046e3bb4b.herokuapp.com/' + "api"
+
+// when you test locally, use the localhost url
+const baseURL = 'http://localhost:4000/api'
+
+const createMap = (mapData, username, mapName) => {
+    return fetch(`${baseURL}/map/`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            name: mapName,
+            mapData: mapData,
+            owner: username
+        }),
+    })
+    .then(response => {
+        // Parse JSON and include status in the resolved value
+        return response.json().then(data => ({ status: response.status, data }));
     })
 }
 
-export const deleteMap = (mapId) => {
-    return api.delete(`/map/${mapId}`)
-}
-
-export const updateMap = (mapId, mapData) => {
-    return api.put(`/map/${mapId}`, {
-        mapData: mapData
-    })
-}
-// get one specific map
-export const getMapById = (mapId) => {
-    return api.get(`/map/${mapId}`)
-}
-
-//get all maps from one user
-export const getMapList = (userId) => {
-    return api.get(`/maplist/${userId}`)
-}
-
-//get all published maps from all users
-export const getPublishedMapList = () => {
-    return api.get(`/published-maplist/`)
-}
-
-//search map by map name in home page
-export const searchMapByName = (mapName, userId) => {
-    return api.get(`/searchmap-map-name/${userId}`, {
-        params: {
-            mapName: mapName
-        }
-    })
-}
-
-//search map by map name in community page
-export const searchMapByNameInCommunity = (mapName) => {
-    return api.get(`/searchmap-map-name-community/`, {
-        params: {
-            mapName: mapName
-        }
-    })
-}
-
-//search map by mapid
-export const searchMapById = (mapId) => {
-    return api.get(`/searchmap-mapid/`, {
-        params: {
-            mapId: mapId
-        }
-    })
-}
-
-//search map by property in home page
-export const searchMapByProperty = (userId, property) => {
-    return api.get(`/searchmap-property/${userId}`, {
-        params: {
-            property: property
-        }
-    })
-}
-
-//search map by property in community page
-export const searchMapByPropertyInCommunity = (property) => {
-    return api.get(`/searchmap-property-community/`, {
-        params: {
-            property: property
-        }
-    })
-}
-
-const apis = {
+export default {
     createMap,
-    deleteMap,
-    updateMap,
-    getMapById,
-    getMapList,
-    getPublishedMapList,
-    searchMapByName,
-    searchMapByNameInCommunity,
-    searchMapById,
-    searchMapByProperty,
-    searchMapByPropertyInCommunity
-}
-
-export default apis;
+};
---
+++ routers/mapRoute.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const MapController = require('../controller/storeController')
+const router = express.Router()
+const auth = require('../auth')
+
+router.post('/map', auth.verify, MapController.createMap)
+router.delete('/map/:id', auth.verify, MapController.deleteMap)
+// router.get('/playlist/:id', auth.verify, PlaylistController.getPlaylistById)
+// router.get('/playlistpairs', auth.verify, PlaylistController.getPlaylistPairs)
+// router.get('/playlists', auth.verify, PlaylistController.getPlaylists)
+router.put('/map/:id', auth.verify, MapController.updateMap)
+router.get('/maps', auth.verify, MapController.getMapsByUser)
+router.get('/map/:id', MapController.getMapById)
+router.get('/published-maps', MapController.getPublishedMaps)
+
+module.exports = router
