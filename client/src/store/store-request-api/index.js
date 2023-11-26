79a13c60c72794719b7cbabb16eb35ca93b18f27
lenzlaww <yuwenqian.chen@stonybrook.edu>
2023-11-25 23:06:30 -0500

    fix it

--- client/src/store/store-request-api/index.js
+++ client/src/store/store-request-api/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+//  修好啦！！！！
 // axios.defaults.withCredentials = true;
 // const baseURL = 'https://patrick-barbie-f64046e3bb4b.herokuapp.com/' + "api"
 // const api = axios.create({
