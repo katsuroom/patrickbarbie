e214347c6f04ffe0865990756a694343c14d0afc
ScottZhengGit <Scott.zheng@stonybrook.edu>
2023-11-27 01:59:55 -0500

    added hardcoded maps

--- client/src/components/MainScreen.js
+++ client/src/components/MainScreen.js
@@ -10,23 +10,22 @@ export default function MainScreen()
 
     const { store } = useContext(StoreContext);
 
+    // const maps = [
+    //     { id: 1, name: 'North America', fileName: "NA2.json"},
+    //     { id: 2, name: 'South America', fileName: "SA2.json"},
+    //     { id: 3, name: 'Asia', fileName: "ASIA2.json"},
+    //     { id: 4, name: 'Africa', fileName: "AFRICA2.json"},
+    //     { id: 5, name: 'Europe', fileName: "EU2.json"},
+    //     { id: 6, name: 'Oceania', fileName: "Oceania2.json"},
+    //     { id: 7, name: 'World', fileName: "World.json"}
+    // ]
 
-    const maps = [
-        { id: 1, name: 'North America', fileName: "NA2.json"},
-        { id: 2, name: 'South America', fileName: "SA2.json"},
-        { id: 3, name: 'Asia', fileName: "ASIA2.json"},
-        { id: 4, name: 'Africa', fileName: "AFRICA2.json"},
-        { id: 5, name: 'Europe', fileName: "EU2.json"},
-        { id: 6, name: 'Oceania', fileName: "Oceania2.json"},
-        { id: 7, name: 'World', fileName: "World.json"}
-    ]
 
-
-    console.log(maps);
+    // console.log(maps);
     return(
         <div>
             <div >
-                <MapCardList maps={maps}/>
+                <MapCardList/>
                 <MapView />
                 {/* <MapDisplay/> */}
             </div>
--- client/src/components/MapCardList.js
+++ client/src/components/MapCardList.js
@@ -1,95 +1,100 @@
-import React, { useState, useContext, useEffect } from "react";
-import { useHistory } from "react-router-dom";
-import Box from "@mui/material/Box";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemText from "@mui/material/ListItemText";
-import Divider from "@mui/material/Divider";
-import Fab from "@mui/material/Fab";
-import AddIcon from "@mui/icons-material/Add";
-import StoreContext, {CurrentModal} from "../store";
-import AuthContext from "../auth";
+import React, { useState, useContext, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
+import Fab from '@mui/material/Fab';
+import AddIcon from '@mui/icons-material/Add';
+import StoreContext, { CurrentModal } from '../store';
+import AuthContext from '../auth';
 
+const hardcodedMaps = [
+  { _id: '1', title: 'North America', fileName: "NA2.json" },
+  { _id: '2', title: 'South America', fileName: "SA2.json" },
+  { _id: '3', title: 'Asia', fileName: "ASIA2.json" },
+  { _id: '4', title: 'Africa', fileName: "AFRICA2.json" },
+  { _id: '5', title: 'Europe', fileName: "EU2.json" },
+  { _id: '6', title: 'Oceania', fileName: "Oceania2.json" },
+  { _id: '7', title: 'World', fileName: "World.json" }
+];
 
-export default function MapCardList(props) {
+export default function MapCardList() {
   const history = useHistory();
   const { store } = useContext(StoreContext);
   const { auth } = useContext(AuthContext);
-  const [maps, setMaps] = useState([]);
+  const [maps, setMaps] = useState([...hardcodedMaps]);
   const [selectedMap, setSelectedMap] = useState(null);
 
   useEffect(() => {
     if (auth.loggedIn && auth.user) {
       const updateMapsInState = (fetchedMaps) => {
-        setMaps(fetchedMaps);
+        setMaps([...hardcodedMaps, ...fetchedMaps]); // Combine hardcoded maps with fetched maps
       };
-
       store.getMapsByUser(updateMapsInState);
     }
   }, [auth.user, auth.loggedIn, store]);
 
+  const handleMapClick = (mapId) => {
+    const selected = maps.find((map) => map._id === mapId);
+    if (selected) {
+      setSelectedMap(selected);
+      store.getMapFile(selected.fileName); // Ensure 'fileName' is a valid property
+    }
+  };
 
   const handleCreateMap = () => {
     store.openModal(CurrentModal.UPLOAD_MAP);
-    // store.openModal("UPLOAD_MAP");
-  };
-
-  const handleMapClick = (mapId) => {
-    const selected = maps.find(map => map.id === mapId);
-    if (selected) {
-      setSelectedMap(selected);
-      store.getMapFile(selected.fileName);
-    }
   };
 
   return (
     <Box
       sx={{
-        width: "25%",
-        bgcolor: "#F7D3E4",
-        float: "left",
-        height: "83vh",
-        position: "relative",
+        width: '25%',
+        bgcolor: '#F7D3E4',
+        float: 'left',
+        height: '83vh',
+        position: 'relative',
       }}
     >
       <List
         component="nav"
         aria-label="map folders"
         sx={{
-          maxHeight: "85vh",
-          overflow: "auto",
-          "&::-webkit-scrollbar": {
-            width: "10px",
+          maxHeight: '85vh',
+          overflow: 'auto',
+          '&::-webkit-scrollbar': {
+            width: '10px',
           },
-          "&::-webkit-scrollbar-thumb": {
-            backgroundColor: "rgba(0,0,0,.1)",
+          '&::-webkit-scrollbar-thumb': {
+            backgroundColor: 'rgba(0,0,0,.1)',
           },
         }}
       >
-        {maps.map((map, index) => (
-          <React.Fragment key={map._id}>
-            {index > 0 && <Divider />}
-            <ListItem button onClick={() => handleMapClick(map._id)}>
-              <ListItemText
-                primary={map.title}
-                style={{
-                  padding: "0px",
-                  backgroundColor: selectedMap && map._id === selectedMap._id ? "#f6c0fa" : "#F7D3E4",
-                }}
-              />
-            </ListItem>
-          </React.Fragment>
-        ))}
+        {maps.map((map, index) => [
+          // Use an array and add unique keys for each child
+          index > 0 && <Divider key={`divider-${map._id}`} />,
+          <ListItem button onClick={() => handleMapClick(map._id)} key={map._id}>
+            <ListItemText
+              primary={map.title}
+              style={{
+                padding: '0px',
+                backgroundColor: selectedMap && map._id === selectedMap._id ? '#f6c0fa' : '#F7D3E4',
+              }}
+            />
+          </ListItem>,
+        ])}
       </List>
       {auth.loggedIn && (
         <Fab
           sx={{
-            position: "absolute",
+            position: 'absolute',
             bottom: 16,
             right: 16,
-            bgcolor: "#ffabd1",
-            "&:hover": {
-              bgcolor: "#ffabd1",
+            bgcolor: '#ffabd1',
+            '&:hover': {
+              bgcolor: '#ffabd1',
             },
           }}
           onClick={handleCreateMap}
@@ -97,16 +102,8 @@ export default function MapCardList(props) {
           <AddIcon />
         </Fab>
       )}
-      {/* <MUIUploadMap />
-      <MUICreateMap />
-      {selectedMap && (
-        <MapView
-          fileSelected={selectedMap.fileSelected}
-          projectName={selectedMap.name}
-          mapType={selectedMap.mapType}
-          views={selectedMap.views}
-        />
-      )} */}
+      {/* Components such as MUIUploadMap, MUICreateMap, and MapView have been commented out
+      since their implementations are not provided. They should be uncommented and adjusted as needed. */}
     </Box>
   );
 }
