ac5bce08287d9bbfa0b0a87bec5067583d96d3bd
ScottZhengGit <Scott.zheng@stonybrook.edu>
2023-11-27 02:15:35 -0500

    maps update

--- client/src/components/MapCardList.js
+++ client/src/components/MapCardList.js
@@ -1,5 +1,4 @@
 import React, { useState, useContext, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
 import Box from '@mui/material/Box';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
@@ -11,17 +10,16 @@ import StoreContext, { CurrentModal } from '../store';
 import AuthContext from '../auth';
 
 const hardcodedMaps = [
-  { _id: '1', title: 'North America', fileName: "NA2.json" },
-  { _id: '2', title: 'South America', fileName: "SA2.json" },
-  { _id: '3', title: 'Asia', fileName: "ASIA2.json" },
-  { _id: '4', title: 'Africa', fileName: "AFRICA2.json" },
-  { _id: '5', title: 'Europe', fileName: "EU2.json" },
-  { _id: '6', title: 'Oceania', fileName: "Oceania2.json" },
-  { _id: '7', title: 'World', fileName: "World.json" }
+  { _id: '1', title: 'North America', fileName: "NA2.json", type: 'hardcoded' },
+  { _id: '2', title: 'South America', fileName: "SA2.json", type: 'hardcoded' },
+  { _id: '3', title: 'Asia', fileName: "ASIA2.json", type: 'hardcoded' },
+  { _id: '4', title: 'Africa', fileName: "AFRICA2.json", type: 'hardcoded' },
+  { _id: '5', title: 'Europe', fileName: "EU2.json", type: 'hardcoded' },
+  { _id: '6', title: 'Oceania', fileName: "Oceania2.json", type: 'hardcoded' },
+  { _id: '7', title: 'World', fileName: "World.json", type: 'hardcoded' }
 ];
 
 export default function MapCardList() {
-  const history = useHistory();
   const { store } = useContext(StoreContext);
   const { auth } = useContext(AuthContext);
   const [maps, setMaps] = useState([...hardcodedMaps]);
@@ -30,7 +28,8 @@ export default function MapCardList() {
   useEffect(() => {
     if (auth.loggedIn && auth.user) {
       const updateMapsInState = (fetchedMaps) => {
-        setMaps([...hardcodedMaps, ...fetchedMaps]); // Combine hardcoded maps with fetched maps
+        const typedFetchedMaps = fetchedMaps.map(map => ({ ...map, type: 'fetched' }));
+        setMaps([...hardcodedMaps, ...typedFetchedMaps]);
       };
       store.getMapsByUser(updateMapsInState);
     }
@@ -40,7 +39,15 @@ export default function MapCardList() {
     const selected = maps.find((map) => map._id === mapId);
     if (selected) {
       setSelectedMap(selected);
-      store.getMapFile(selected.fileName); // Ensure 'fileName' is a valid property
+      if (selected.type === 'hardcoded') {
+        // hardcoded maps
+        setSelectedMap(selected);
+        store.getMapFile(selected.fileName);
+        console.log('Hardcoded map clicked:', selected.title);
+      } else if (selected.type === 'fetched') {
+        // fetched map click
+        store.getMapFile(selected.fileName);
+      }
     }
   };
 
@@ -73,7 +80,6 @@ export default function MapCardList() {
         }}
       >
         {maps.map((map, index) => [
-          // Use an array and add unique keys for each child
           index > 0 && <Divider key={`divider-${map._id}`} />,
           <ListItem button onClick={() => handleMapClick(map._id)} key={map._id}>
             <ListItemText
@@ -102,8 +108,6 @@ export default function MapCardList() {
           <AddIcon />
         </Fab>
       )}
-      {/* Components such as MUIUploadMap, MUICreateMap, and MapView have been commented out
-      since their implementations are not provided. They should be uncommented and adjusted as needed. */}
     </Box>
   );
 }
