96b0f7aa29424ecc6159e271a906ddd6bc08ac80
lenzlaww <yuwenqian.chen@stonybrook.edu>
2023-12-03 05:15:52 -0500

    fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, {useEffect, useState  } from 'react'
+import React from 'react'
 import { BrowserRouter, Route, Switch  } from 'react-router-dom'
 import { AuthContextProvider } from './auth';
 import { StoreContextProvider } from './store';
@@ -8,13 +8,6 @@ import {
     LoginScreen,
     RegisterScreen,
     PasswordRecoveryPage,
-    MUIPublishMap,
-    MUIDeleteMap,
-    MUISaveChanges,
-    MUIForkMap,
-    MUIUploadMap,
-    MUICreateMap,
-    MUIExportMap,
     PHeatmap,
     Sort,
     MUIExit,
@@ -39,13 +32,6 @@ const App = () => {
                     <Route path="/register/" exact component={RegisterScreen} />
                     <Route path="/recovery/" exact component={PasswordRecoveryPage} />
                     <Route path="/main/" exact component={MainScreen} />
-                    <Route path="/publishMap/" exact component={MUIPublishMap} />
-                    <Route path="/deleteMap/" exact component={MUIDeleteMap} />
-                    {/* <Route path="/saveMap/" exact component={MUISaveChanges} /> */}
-                    <Route path="/forkMap/" exact component={MUIForkMap} />
-                    <Route path="/uploadMap/" exact component={MUIUploadMap} />
-                    <Route path="/createMap/" exact component={MUICreateMap} />
-                    <Route path="/exportMap/" exact component={MUIExportMap} />
                     <Route path="/heatmap/" exact component={PHeatmap} />
                     <Route path="/sort/" exact component={Sort} />
                     <Route path="/MUIExit/" exact component={MUIExit} />
