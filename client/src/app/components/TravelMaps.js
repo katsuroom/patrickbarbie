70d53d69622127764db9ee2b63fbc39033ec4844
Weng-Sean <143642405+Weng-Sean@users.noreply.github.com>
2023-12-04 13:30:09 -0500

    typo fixed

--- client/src/app/components/TravelMaps.js
+++ client/src/app/components/TravelMaps.js
@@ -1,7 +1,7 @@
 "use client"
 
 import React, { useEffect, useState, useRef, useContext } from 'react';
-import './TravelMap.css';
+import './travelmap.css';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import 'leaflet-routing-machine';
