53dcf57667c19cf786b798acb2af54a0efebc9f9
ScottZhengGit <Scott.zheng@stonybrook.edu>
2023-12-04 14:24:12 -0500

    fix

--- cypress/e2e/mapEditSaveSave.cy.js
+++ cypress/e2e/mapEditSaveSave.cy.js
@@ -1,9 +1,16 @@
+
 describe("template spec", () => {
     Cypress.on('uncaught:exception', (err, runnable) => {
         return false
     })
+    Cypress.on('uncaught:exception', (err, runnable) => {
+        return false
+    })
     it("open EU map and click delete then cancel", () => {
+
+
         cy.visit("https://patrick-barbie-f64046e3bb4b.herokuapp.com/");
+        // cy.visit("http://localhost:3000");
 
         cy.wait(500);
 
@@ -22,11 +29,13 @@ describe("template spec", () => {
 
         cy.wait(500);
 
-        cy.get('.forkButton').click();
+        cy.get('.editButton').click();
 
         cy.wait(500);
 
-        cy.get(".modal-button-fork-cancel").click();
+        cy.contains("SAVE").click();
+
+        cy.wait(500);
 
     });
 });
