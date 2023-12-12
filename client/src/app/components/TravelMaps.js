fcf4b9136b8456355323eeb29d22bd0ad55d2721
Weng-Sean <143642405+Weng-Sean@users.noreply.github.com>
2023-12-12 15:59:35 -0500

    conflict resolved

--- client/src/app/components/TravelMaps.js
+++ client/src/app/components/TravelMaps.js
@@ -1,59 +1,53 @@
-import React, { useEffect, useState, useRef, useContext } from "react";
-import "./travelmap.css";
-import L from "leaflet";
-import "leaflet/dist/leaflet.css";
-import "leaflet-routing-machine";
+import React, { useEffect, useState, useRef, useContext } from 'react';
+import './travelmap.css';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import 'leaflet-routing-machine';
 
 // import endIconUrl from './red.png';
 // import startIconUrl from './blue.png';
 import StoreContext, { CurrentModal } from "@/store";
-import "leaflet-contextmenu";
-import "leaflet-contextmenu/dist/leaflet.contextmenu.css";
-import "leaflet-control-geocoder";
+import 'leaflet-contextmenu';
+import 'leaflet-contextmenu/dist/leaflet.contextmenu.css';
+import 'leaflet-control-geocoder';
 import Button from "@mui/joy/Button";
-import "leaflet-control-geocoder/dist/Control.Geocoder.css";
+import 'leaflet-control-geocoder/dist/Control.Geocoder.css';
+
 
 const TravelMap = (props) => {
-  // const [start, setStart] = useState('');
-  // const [end, setEnd] = useState('');
-  const mapRef = useRef(null);
+    // const [start, setStart] = useState('');
+    // const [end, setEnd] = useState('');
+    const mapRef = useRef(null);
 
-  const routeControlRef = useRef(null);
-  const { store } = useContext(StoreContext);
-  const geoJsonLayerRef = useRef(null);
-  const markers = useRef([]);
-  const [loadScripts, setLoadScripts] = useState(false);
+    const routeControlRef = useRef(null);
+    const { store } = useContext(StoreContext);
+    const geoJsonLayerRef = useRef(null);
+    const markers = useRef([]);
+    const [loadScripts, setLoadScripts] = useState(false);
 
-  const mapLayerRef = useRef(null);
-  const hybridLayerRef = useRef(null);
-  const satelliteLayerRef = useRef(null);
-  const darkLayerRef = useRef(null);
-  const lightLayerRef = useRef(null);
-  const settingLayerRef = useRef(null);
+    const mapLayerRef = useRef(null);
+    const hybridLayerRef = useRef(null);
+    const satelliteLayerRef = useRef(null);
+    const darkLayerRef = useRef(null);
+    const lightLayerRef = useRef(null);
+    const settingLayerRef = useRef(null);
 
-  const startHere = (e) => {
-    if (routeControlRef.current) {
-      routeControlRef.current.spliceWaypoints(0, 1, e.latlng);
-      console.log(
-        "routeControlRef.current.getWaypoints" +
-          routeControlRef.current.getWaypoints().map((wp) => wp.latLng)
-      );
-      console.log(
-        "routeControlRef.current.Waypoints" + routeControlRef.current.Waypoints
-      );
-    }
-  };
 
-  const goHere = (e) => {
-    if (routeControlRef.current) {
-      routeControlRef.current.spliceWaypoints(
-        routeControlRef.current.getWaypoints().length - 1,
-        1,
-        e.latlng
-      );
-      // store.setWaypoints(routeControlRef.current.getWaypoints());
-    }
-  };
+
+    const startHere = (e) => {
+        if (routeControlRef.current) {
+            routeControlRef.current.spliceWaypoints(0, 1, e.latlng);
+            console.log('routeControlRef.current.getWaypoints' + routeControlRef.current.getWaypoints().map(wp => wp.latLng))
+            console.log('routeControlRef.current.Waypoints' + routeControlRef.current.Waypoints)
+        }
+    };
+
+    const goHere = (e) => {
+        if (routeControlRef.current) {
+            routeControlRef.current.spliceWaypoints(routeControlRef.current.getWaypoints().length - 1, 1, e.latlng);
+            // store.setWaypoints(routeControlRef.current.getWaypoints());
+        }
+    };
 
     const loadScript = (src) => {
         return new Promise((resolve, reject) => {
@@ -192,133 +186,125 @@ const TravelMap = (props) => {
         // }).addTo(mapRef.current)
     }
 
-  useEffect(() => {
-    refreshMap();
-  }, [loadScripts]);
+    useEffect(() => {
+        refreshMap();
+    }, [loadScripts]);
 
-  useEffect(() => {
-    store.waypoints = [];
-    if (store.currentMapObject.mapProps?.waypoints) {
-      console.log(
-        "store.currentMapObject.mapProps" +
-          store.currentMapObject.mapProps.waypoints
-      );
-      store.setWaypoints(store.currentMapObject.mapProps.waypoints);
-      runDirection();
-    }
-  }, [store.currentMapObject]);
+    useEffect(() => {
+        store.waypoints = []
+        if (store.currentMapObject.mapProps?.waypoints) {
+            console.log("store.currentMapObject.mapProps" + store.currentMapObject.mapProps.waypoints)
+            store.setWaypoints(store.currentMapObject.mapProps.waypoints)
+            runDirection();
+        }
+    }, [store.currentMapObject]);
 
-  // const openSaveModal = () => store.openModal(CurrentModal.SAVE_EDIT);
-  // const openExitModal = () => store.openModal(CurrentModal.EXIT_EDIT);
+    // const openSaveModal = () => store.openModal(CurrentModal.SAVE_EDIT);
+    // const openExitModal = () => store.openModal(CurrentModal.EXIT_EDIT);
 
-  const runDirection = async () => {
-    // const runDirection = async (start, end) => {
-    const geocode = async (address) => {
-      const url = `https://www.mapquestapi.com/geocoding/v1/address?key=S8d7L47mdyAG5nHG09dUnSPJjreUVPeC&location=${encodeURIComponent(
-        address
-      )}`;
-      const response = await fetch(url);
-      const data = await response.json();
-      const location = data.results[0].locations[0].latLng;
-      return location;
+    const runDirection = async () => {
+
+        // const runDirection = async (start, end) => {
+        const geocode = async (address) => {
+            const url = `https://www.mapquestapi.com/geocoding/v1/address?key=S8d7L47mdyAG5nHG09dUnSPJjreUVPeC&location=${encodeURIComponent(address)}`;
+            const response = await fetch(url);
+            const data = await response.json();
+            const location = data.results[0].locations[0].latLng;
+            return location;
+        };
+
+        try {
+            // const startPoint = await geocode(start);
+            // const endPoint = await geocode(end);
+
+            const startIcon = L.icon({
+                iconUrl: "/blue.png",
+                iconSize: [25, 41],
+                iconAnchor: [12, 41]
+            });
+
+            const inBetweenIcon = L.icon({
+                iconUrl: "/gray.png",
+                iconSize: [25, 41],
+                iconAnchor: [12, 41]
+            });
+
+            const endIcon = L.icon({
+                iconUrl: "/red.png",
+                iconSize: [25, 41],
+                iconAnchor: [12, 41]
+            });
+
+            if (routeControlRef.current) {
+                mapRef.current.removeControl(routeControlRef.current);
+            }
+
+            const routingControl = L.Routing.control({
+                waypoints: store.waypoints,
+                createMarker: function (i, waypoint, n) {
+                    const markerIcon = i === 0 ? startIcon : (i > 0 && i < n - 1) ? inBetweenIcon : endIcon;
+                    return L.marker(waypoint.latLng, { icon: markerIcon });
+                },
+                routeWhileDragging: false,
+                addWaypoints: false,
+            });
+
+            routingControl.on('waypointschanged', function (e) {
+                const updatedWaypoints = e.waypoints;
+                console.log('Waypoints Updated:', updatedWaypoints);
+                store.setWaypoints(updatedWaypoints.map(p => {
+                    return p.latLng
+                }));
+            });
+
+            // Add the routing control to the map
+            routingControl.addTo(mapRef.current);
+            // .on('routingstart', showSpinner)
+            // .on('routesfound routingerror', hideSpinner)
+            // .addTo(mapRef.current);
+
+            routeControlRef.current = routingControl;
+
+            // mapRef.current.forEach((routingControl) => {
+            //     mapRef.current.removeLayer(routingControl);
+            // });
+        } catch (error) {
+            console.error('Error in geocoding or routing:', error);
+        }
     };
 
-    try {
-      // const startPoint = await geocode(start);
-      // const endPoint = await geocode(end);
+    // var spinner = true;
+    // const showSpinner = ()=>{
+    //     if(spinner){
+    //         document.getElementById('loader').style.display = "block";
+    //     }
+    // }
+    // const hideSpinner = ()=>{
+    //         document.getElementById('loader').style.display = "none";
+    // }
 
-      const startIcon = L.icon({
-        iconUrl: "/blue.png",
-        iconSize: [25, 41],
-        iconAnchor: [12, 41],
-      });
 
-      const inBetweenIcon = L.icon({
-        iconUrl: "/gray.png",
-        iconSize: [25, 41],
-        iconAnchor: [12, 41],
-      });
+    const submitForm = (event) => {
+        event.preventDefault();
+        // runDirection(start, end);
+        setStart('');
+        setEnd('');
+    };
 
-      const endIcon = L.icon({
-        iconUrl: "/red.png",
-        iconSize: [25, 41],
-        iconAnchor: [12, 41],
-      });
-
-      if (routeControlRef.current) {
-        mapRef.current.removeControl(routeControlRef.current);
-      }
-
-      const routingControl = L.Routing.control({
-        waypoints: store.waypoints,
-        createMarker: function (i, waypoint, n) {
-          const markerIcon =
-            i === 0 ? startIcon : i > 0 && i < n - 1 ? inBetweenIcon : endIcon;
-          return L.marker(waypoint.latLng, { icon: markerIcon });
-        },
-        routeWhileDragging: false,
-        addWaypoints: false,
-      });
-
-      routingControl.on("waypointschanged", function (e) {
-        const updatedWaypoints = e.waypoints;
-        console.log("Waypoints Updated:", updatedWaypoints);
-        store.setWaypoints(
-          updatedWaypoints.map((p) => {
-            return p.latLng;
-          })
-        );
-      });
-
-      // Add the routing control to the map
-      routingControl.addTo(mapRef.current);
-      // .on('routingstart', showSpinner)
-      // .on('routesfound routingerror', hideSpinner)
-      // .addTo(mapRef.current);
-
-      routeControlRef.current = routingControl;
-
-      // mapRef.current.forEach((routingControl) => {
-      //     mapRef.current.removeLayer(routingControl);
-      // });
-    } catch (error) {
-      console.error("Error in geocoding or routing:", error);
-    }
-  };
-
-  // var spinner = true;
-  // const showSpinner = ()=>{
-  //     if(spinner){
-  //         document.getElementById('loader').style.display = "block";
-  //     }
-  // }
-  // const hideSpinner = ()=>{
-  //         document.getElementById('loader').style.display = "none";
-  // }
-
-  const submitForm = (event) => {
-    event.preventDefault();
-    // runDirection(start, end);
-    setStart("");
-    setEnd("");
-  };
-
-  return (
-    <div>
-      <div
-        id={"map-display"}
-        style={{ height: `${mapHeight}px`, margin: "10px" }}
-      ></div>
-      {/* <div id={"map-display"} style={{ width: "99vw", height: `${mapHeight}px`, margin: '10px' }}></div> */}
-      {/* <div id={"loader"} style={{ height: `5px`, margin: '5px' }}></div> */}
-      {/* <Button variant="solid" className="exit" sx={{ margin: 1 }} onClick={openExitModal}>
+    return (
+        <div>
+            <div id={"map-display"} style={{ height: `${mapHeight}px`, margin: '10px' }}></div>
+            {/* <div id={"map-display"} style={{ width: "99vw", height: `${mapHeight}px`, margin: '10px' }}></div> */}
+            {/* <div id={"loader"} style={{ height: `5px`, margin: '5px' }}></div> */}
+            {/* <Button variant="solid" className="exit" sx={{ margin: 1 }} onClick={openExitModal}>
                 EXIT
             </Button>
             <Button variant="solid" className="save" sx={{ margin: 1 }} onClick={openSaveModal}>
                 SAVE
             </Button> */}
-    </div>
-  );
+        </div>
+
+    );
 };
 
 export default TravelMap;
