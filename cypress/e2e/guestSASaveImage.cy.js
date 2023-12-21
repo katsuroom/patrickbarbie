ff8abdada7dc6c7a8390761cb4c489913793016b
katsuroom <kerrance.dai@gmail.com>
2023-12-20 22:12:29 -0500

    cypress test adjustments

--- cypress/e2e/guestSASaveImage.cy.js
+++ cypress/e2e/guestSASaveImage.cy.js
@@ -15,9 +15,7 @@ describe('template spec', () => {
 
         cy.wait(10000);
 
-        cy.wait(10000);
-
-cy.get("[id*='map-card']").first().click();
+        cy.get("[id*='map-card']").first().click();
 
         cy.wait(1000); 
 
